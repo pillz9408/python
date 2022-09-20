5c199d5fa5769fbca75ed9e59155c28e4b3df28b
pillz9408 <pillz9408@gmail.com>
2022-09-20 10:47:43 +0900

    sd

---
+++ DataVisualize/module_test.py
@@ -0,0 +1,17 @@
+# %%
+def add(num1,num2):
+    return num1 + num2
+
+# %%
+def minus(num1,num2):
+    return num1 - num2
+
+# %%
+def multiple(num1,num2):
+    return num1 * num2
+
+# %%
+def divided(num1,num2):
+    return num1 / num2
+
+
